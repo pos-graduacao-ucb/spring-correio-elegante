25eaf3d0cb241c3aff9dc268bb29e53bb2ddc64f
Gabriela <zooiv3rde@gmail.com>
2019-08-20 21:50:15 -0300

    commit inicial

---
+++ src/main/java/br/com/guedes/elegantez/ElegantezApplication.java
@@ -0,0 +1,13 @@
+package br.com.guedes.elegantez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegantezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElegantezApplication.class, args);
+	}
+
+}
