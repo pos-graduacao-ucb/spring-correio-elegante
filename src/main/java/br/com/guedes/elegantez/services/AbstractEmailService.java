acbc3833130a2870a291d5a149bfb313da97f6aa
Gabriela <zooiv3rde@gmail.com>
2019-08-31 14:58:42 -0300

    create abstract email service

---
+++ src/main/java/br/com/guedes/elegantez/services/AbstractEmailService.java
@@ -0,0 +1,27 @@
+package br.com.guedes.elegantez.services;
+
+import br.com.guedes.elegantez.domain.User;
+import br.com.guedes.elegantez.services.impl.EmailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.TemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+public class AbstractEmailService implements EmailServiceImpl {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    public void emailDeConfirmacaoHtml(User user) throws MessagingException {
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage mimeMessage) {
+    }
+}
