91d29effbf73ea404a9c6ef0a1cf5802b56a6b80
Gabriela <zooiv3rde@gmail.com>
2019-09-28 17:14:06 -0300

    find all elegant mails

--- src/main/java/br/com/guedes/elegantez/ElegantezApplication.java
+++ src/main/java/br/com/guedes/elegantez/ElegantezApplication.java
@@ -1,7 +1,9 @@
 package br.com.guedes.elegantez;
 
+import br.com.guedes.elegantez.domain.ElegantMail;
 import br.com.guedes.elegantez.domain.User;
 import br.com.guedes.elegantez.domain.enums.Profile;
+import br.com.guedes.elegantez.repositories.ElegantMailRepository;
 import br.com.guedes.elegantez.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,7 +17,10 @@ import java.util.Arrays;
 public class ElegantezApplication implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
+
+	@Autowired
+	private ElegantMailRepository elegantMailRepository;
 
 	@Autowired
 	private BCryptPasswordEncoder bc;
@@ -30,8 +35,12 @@ public class ElegantezApplication implements CommandLineRunner {
 		User gabriela = new User(null, "Gabriela", "azooiverde", "zooiv3rde@gmail.com", bc.encode("123"), "23", "feminino");
 		gabriela.addProfile(Profile.ADMIN);
 
-		repository.deleteAll();
-		repository.saveAll(Arrays.asList(gabriela));
+		ElegantMail myCorreioElegante = new ElegantMail(null, "É muito programar, na verdade é muito bom fazer o que gostamos!!");
+
+		elegantMailRepository.deleteAll();;
+		userRepository.deleteAll();
+		elegantMailRepository.saveAll(Arrays.asList(myCorreioElegante));
+		userRepository.saveAll(Arrays.asList(gabriela));
 	}
 
 }
--- src/main/java/br/com/guedes/elegantez/config/SecurityConfig.java
+++ src/main/java/br/com/guedes/elegantez/config/SecurityConfig.java
@@ -33,8 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     };
 
     private static final String[] PUBLIC_MATCHERS_GET = {
-            "/elegantEmail/**",
-
+            "/mails/**",
     };
 
     @Override
