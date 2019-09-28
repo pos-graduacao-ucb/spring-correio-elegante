6c646f0ecd716275cd81e36164acc8e69d8de160
Gabriela <zooiv3rde@gmail.com>
2019-09-28 16:55:08 -0300

    statrt resource mails

---
+++ src/main/java/br/com/guedes/elegantez/resources/ElegantMailResource.java
@@ -0,0 +1,9 @@
+package br.com.guedes.elegantez.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/mails")
+public class ElegantMailResource {
+}
