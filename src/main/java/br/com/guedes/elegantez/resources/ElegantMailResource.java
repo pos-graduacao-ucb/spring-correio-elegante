bf8fa14c7cf1f61af5aa286fdf3082a6cc7fec8e
Gabriela <zooiv3rde@gmail.com>
2019-10-12 20:00:07 -0300

    resource finish

--- src/main/java/br/com/guedes/elegantez/resources/ElegantMailResource.java
+++ src/main/java/br/com/guedes/elegantez/resources/ElegantMailResource.java
@@ -4,9 +4,11 @@ import br.com.guedes.elegantez.domain.ElegantMail;
 import br.com.guedes.elegantez.domain.User;
 import br.com.guedes.elegantez.services.ElegantMailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +24,14 @@ public class ElegantMailResource {
     public ResponseEntity<List<ElegantMail>> findAll() {
         List<ElegantMail> mailList = service.findAll();
         return ResponseEntity.ok().body(mailList);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Page<ElegantMail>> findPage(
+            @RequestParam(value="page", defaultValue="0") Integer page,
+            @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+            @RequestParam(value="orderBy", defaultValue="instante") String orderBy,
+            @RequestParam(value="direction", defaultValue="ASC") String direction) {
+        Page<ElegantMail> elegantMailList = service.findPage(page, linesPerPage, orderBy, direction);
+        return ResponseEntity.ok().body(elegantMailList);
     }
 }
