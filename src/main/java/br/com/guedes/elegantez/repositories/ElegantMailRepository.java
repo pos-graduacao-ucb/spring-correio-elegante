2dc03b945402fa9b32e6baa6b3dbdf415d37d328
Gabriela <zooiv3rde@gmail.com>
2019-10-12 19:47:32 -0300

    finish service of pageable

--- src/main/java/br/com/guedes/elegantez/repositories/ElegantMailRepository.java
+++ src/main/java/br/com/guedes/elegantez/repositories/ElegantMailRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 public interface ElegantMailRepository extends MongoRepository<ElegantMail, String> {
 
     @Transactional(readOnly = true)
-    Page<ElegantMail> findByUser(User user, Pageable pageRequest);
+    Page<ElegantMail> findByUser(Optional<User> user, Pageable pageRequest);
 }
--- src/main/java/br/com/guedes/elegantez/services/ElegantMailService.java
+++ src/main/java/br/com/guedes/elegantez/services/ElegantMailService.java
@@ -1,7 +1,11 @@
 package br.com.guedes.elegantez.services;
 
 import br.com.guedes.elegantez.domain.ElegantMail;
+import br.com.guedes.elegantez.domain.User;
 import br.com.guedes.elegantez.repositories.ElegantMailRepository;
+import br.com.guedes.elegantez.repositories.UserRepository;
+import br.com.guedes.elegantez.security.UserSS;
+import br.com.guedes.elegantez.services.exceptions.AuthorizationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +13,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ElegantMailService {
@@ -16,12 +21,23 @@ public class ElegantMailService {
     @Autowired
     private ElegantMailRepository repository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public List<ElegantMail> findAll() {
         return repository.findAll();
     }
 
     public Page<ElegantMail> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        UserSS userSS = UserService.authenticated();
+
+        if(userSS == null) {
+            throw new AuthorizationException("Acesso negado");
+        }
+
         PageRequest pageRequest = new PageRequest(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-        return repository.findAll(pageRequest);
+        Optional<User> user = userRepository.findById(userSS.getId());
+
+        return repository.findByUser(user, pageRequest);
     }
 }
