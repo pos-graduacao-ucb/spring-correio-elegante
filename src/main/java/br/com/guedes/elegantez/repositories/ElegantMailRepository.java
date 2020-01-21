5df506cf9ed633a659c7bf7010951eb1d12731ff
Gabriela <zooiv3rde@gmail.com>
2020-01-21 05:18:47 -0300

    remove

--- src/main/java/br/com/guedes/elegantez/repositories/ElegantMailRepository.java
+++ src/main/java/br/com/guedes/elegantez/repositories/ElegantMailRepository.java
@@ -1,17 +1,10 @@
 package br.com.guedes.elegantez.repositories;
 
 import br.com.guedes.elegantez.domain.ElegantMail;
-import br.com.guedes.elegantez.domain.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ElegantMailRepository extends MongoRepository<ElegantMail, String> {
-
-//    @Transactional(readOnly = true)
-//    Page<ElegantMail> findByElegantMail(User user, Pageable pageRequest);
 
 }
