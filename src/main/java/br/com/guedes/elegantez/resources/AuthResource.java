83991f2da5af89411f6b72f4bcc298625e3ac80c
Gabriela <zooiv3rde@gmail.com>
2020-01-21 05:57:58 -0300

    refresh token

---
+++ src/main/java/br/com/guedes/elegantez/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package br.com.guedes.elegantez.resources;
+
+import br.com.guedes.elegantez.security.UserSS;
+import br.com.guedes.elegantez.services.UserService;
+import br.com.guedes.elegantez.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
