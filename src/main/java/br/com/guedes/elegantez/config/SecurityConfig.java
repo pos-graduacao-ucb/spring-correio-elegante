ff6e90f931e822bd5fba4e50e88719898ebb1689
Gabriela <zooiv3rde@gmail.com>
2020-01-22 21:14:29 -0300

    add config securitu ant matchers permitall for forgot

--- src/main/java/br/com/guedes/elegantez/config/SecurityConfig.java
+++ src/main/java/br/com/guedes/elegantez/config/SecurityConfig.java
@@ -36,13 +36,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/mails/**",
     };
 
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/auth/forgot/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-                .antMatchers(PUBLIC_MATCHERS)
-                .permitAll()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest()
                 .authenticated();
 
