981c6a34230b173d03d07ac5943c8d290d2427d4
Gabriela <zooiv3rde@gmail.com>
2020-01-22 21:01:25 -0300

    create new password, service

---
+++ src/main/java/br/com/guedes/elegantez/security/AuthService.java
@@ -0,0 +1,56 @@
+package br.com.guedes.elegantez.security;
+
+import br.com.guedes.elegantez.domain.User;
+import br.com.guedes.elegantez.repositories.UserRepository;
+import br.com.guedes.elegantez.services.impl.EmailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private BCryptPasswordEncoder bcrypt;
+
+    @Autowired
+    private UserRepository repository;
+
+    private EmailServiceImpl emailService;
+
+    private Random random = new Random();
+
+    public void sendNewPassword(String email) {
+        User user = repository.findByEmail(email);
+        if (user == null) {
+            throw new NullPointerException("Email não encontrado");
+        }
+
+        String newPassword = newPassword();
+        user.setPassword(bcrypt.encode(newPassword));
+        repository.save(user);
+
+        emailService.sendNewPasswordEmail(user, newPassword);
+    }
+
+    private String newPassword() {
+        char[] vetor = new char[10];
+        for (int i = 0; i<10; i++) {
+            vetor[i] = randomChar();
+        }
+        return new String(vetor);
+    }
+
+    private char randomChar() {
+        int option = random.nextInt(3);
+        if(option == 0) {
+            return (char) (random.nextInt(10) + 48);
+        } else if(option == 1) {
+            return (char) (random.nextInt(26) + 65);
+        } else {
+            return (char) (random.nextInt(26) + 95);
+        }
+    }
+}
--- src/main/java/br/com/guedes/elegantez/services/impl/EmailServiceImpl.java
+++ src/main/java/br/com/guedes/elegantez/services/impl/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.guedes.elegantez.services.impl;
 
 import br.com.guedes.elegantez.domain.User;
+import org.springframework.mail.SimpleMailMessage;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -8,5 +9,7 @@ import javax.mail.internet.MimeMessage;
 public interface EmailServiceImpl {
 
     void confirmationEmailHtml(User user) throws MessagingException;
-    void sendEmailHtml(MimeMessage mimeMessage);
+    void sendEmailHtml(SimpleMailMessage simpleMailMessage);
+
+    void sendNewPasswordEmail(User user, String newPassword);
 }
