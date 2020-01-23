d68764771ee319094480bcd5e683e7730ebde3b5
Gabriela <zooiv3rde@gmail.com>
2020-01-22 21:05:33 -0300

    create email dto

---
+++ src/main/java/br/com/guedes/elegantez/dto/EmailDTO.java
@@ -0,0 +1,16 @@
+package br.com.guedes.elegantez.dto;
+
+public class EmailDTO {
+
+    private String email;
+
+    public EmailDTO() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
