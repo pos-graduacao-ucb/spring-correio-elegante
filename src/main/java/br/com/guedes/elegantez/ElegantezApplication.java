b27d37cd3a0aa5075ba25b5ff760e893cdc8f2a1
Gabriela <zooiv3rde@gmail.com>
2020-01-30 22:08:17 -0300

    init config docker

--- src/main/java/br/com/guedes/elegantez/ElegantezApplication.java
+++ src/main/java/br/com/guedes/elegantez/ElegantezApplication.java
@@ -10,11 +10,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@RestController
 public class ElegantezApplication implements CommandLineRunner {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	@Autowired
 	private UserRepository userRepository;
